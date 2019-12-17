dda1fc3d4f8cd585ce5e2a9a325b17cb4b025084
Kevin Downs <kevin.downs04@gmail.com>
2019-12-17 05:26:21 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -14,7 +14,7 @@ class Student
       INSERT INTO students (name, grade)
       VALUES (?,?)
     SQL
-    DB[:conn].execute()
+    DB[:conn].execute(sql, self.name, self.grade)
   end
   
   def self.create_table
